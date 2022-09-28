d2930d7bf199a3fc67f3bed7d9cd9f651fb34daa
sakthiRathinam <sakthiratnam050@gmail.com>
2022-09-28 17:52:26 +0530

    plotted idea to convert dict

--- slacknotifier/slack_fields.py
+++ slacknotifier/slack_fields.py
@@ -1,3 +1,27 @@
-class BaseBlock(object):
-    def __init__(self,name,color):
-        print("imhre")
+import requests
+import logging
+from .constants import *
+
+
+class SlackError(requests.exceptions.RequestException):
+    pass
+
+class DictConvertibleObject:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def to_dict(self):
+        raise NotImplementedError(
+            'Object "{}" does not implemented "to_dict" method'.format(
+                self.__class__.__name__),
+        )
+        
+
+class BaseBlock(DictConvertibleObject):
+    def __init__(self,text_type,text):
+        self.type = text_type
+        self.text = text
+    def to_dict(self):
+        assert if self.type or self.text is None \
+            
